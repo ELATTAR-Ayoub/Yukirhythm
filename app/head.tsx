8143bce0bbb5e957c5bdc6a8cd455241166b84dc
ELATTAR-Ayoub <elattarayoub000@gmail.com>
2023-11-03 19:50:23 +0100

    Upgrade to Next14

--- app/head.tsx
+++ app/head.tsx
@@ -3,7 +3,7 @@ export default function Head() {
     <>
       <title>Ultimate Starter Repo</title>
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta name="description" content="Front-end developer and 3D designer." />
+      <meta name="description" content="Ultimate Starter Repo." />
       <link rel="icon" href="/svgs/logo.svg" type="image/svg+xml" />
     </>
   );
