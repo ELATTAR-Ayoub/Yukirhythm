a78b0cee68f99e7508a5b2ff9af0468149d24231
ELATTAR-Ayoub <elattarayoub000@gmail.com>
2024-09-18 04:32:43 +0100

    make credits better

--- app/credits/page.tsx
+++ app/credits/page.tsx
@@ -14,44 +14,47 @@ import { headerLinks, socialLinks } from "@/constants/index";
 // components
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Badge } from "@/components/ui/badge";
+import { AuroraBackground } from "@/components/ui/aurora-background";
 
 const Page = () => {
   return (
     <section
       className={`  relative ${styles.flexCenter} w-full h-screen ${styles.paddings} `}
     >
-      <section
-        className={` relative w-full max-w-max ${styles.flexStart} flex-col gap-6 rounded-xl border bg-card text-card-foreground shadow p-6 shadow-xl `}
-      >
-        <div className={`${styles.flexStart} gap-3 w-full`}>
-          <Avatar className=" border">
-            <AvatarImage
-              src={
-                "https://media.licdn.com/dms/image/v2/D4E03AQHwA6o49-teGw/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1687049429624?e=1732147200&v=beta&t=nOM9Ut8vO1G68M0tm8YXgtY_RRIHDRBokPlLnmTI-Jo"
-              }
-            />
-            <AvatarFallback>EA</AvatarFallback>
-          </Avatar>
-          <div className={` ${styles.flexStart} text-primary flex-col`}>
-            <h1 className={`${styles.small} font-semibold`}>Elattar Ayoub</h1>
-            <p className={` ${styles.small} text-muted-foreground`}>
-              Web & Brand Designer ✨
-            </p>
+      <AuroraBackground className=" p-4">
+        <section
+          className={` relative w-full max-w-max ${styles.flexStart} flex-col gap-6 rounded-xl border bg-card text-card-foreground shadow p-6 shadow-xl `}
+        >
+          <div className={`${styles.flexStart} gap-3 w-full`}>
+            <Avatar className=" border">
+              <AvatarImage
+                src={
+                  "https://media.licdn.com/dms/image/v2/D4E03AQHwA6o49-teGw/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1687049429624?e=1732147200&v=beta&t=nOM9Ut8vO1G68M0tm8YXgtY_RRIHDRBokPlLnmTI-Jo"
+                }
+              />
+              <AvatarFallback>EA</AvatarFallback>
+            </Avatar>
+            <div className={` ${styles.flexStart} text-primary flex-col`}>
+              <h1 className={`${styles.small} font-semibold`}>Elattar Ayoub</h1>
+              <p className={` ${styles.small} text-muted-foreground`}>
+                Web & Brand Designer ✨
+              </p>
+            </div>
           </div>
-        </div>
 
-        <div className={`${styles.flexStart} gap-2 w-full flex-wrap`}>
-          {socialLinks.map((link, index) => (
-            <Link href={link.url} target="_">
-              <Badge className=" capitalize">{link.name}</Badge>
-            </Link>
-          ))}
-        </div>
+          <div className={`${styles.flexStart} gap-2 w-full flex-wrap`}>
+            {socialLinks.map((link, index) => (
+              <Link href={link.url} target="_">
+                <Badge className=" capitalize">{link.name}</Badge>
+              </Link>
+            ))}
+          </div>
 
-        <span
-          className={` ${styles.XXsmall} text-muted-foreground`}
-        >{`Built with <3 by ELATTAR Ayoub.`}</span>
-      </section>
+          <span
+            className={` ${styles.XXsmall} text-muted-foreground`}
+          >{`Built with <3 by ELATTAR Ayoub.`}</span>
+        </section>
+      </AuroraBackground>
     </section>
   );
 };
